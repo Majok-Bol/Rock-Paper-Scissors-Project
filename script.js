fd633a7d0feb065ab0b649aad45ba735ef5aab34
Majok-Bol <bolmajok001@gmail.com>
2023-11-23 18:07:18 +0300

    added comments to section of the playerSelection and computSelection paramters

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 function rockPaperScissors(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
+  playerSelection =
+    playerSelection.toLowerCase(); /* playerSelection is made case insensitive to enable the player input choice such as rock,Rock,ROCK,RocK etc */
   if (playerSelection === "rock" && computerSeleciton === "scissors") {
-    return "You win";
+    return "You win"; //rock beats scissors
   } else if (playerSelection === "paper" && computerSeleciton === "rock") {
-    return "You win";
+    return "You win"; // paper beats rock
   } else if (playerSelection === "scissors" && computerSeleciton === "paper") {
-    return "You win";
+    return "You win"; // scissors beats paper
   } else if (playerSelection === "rock" && computerSeleciton === "scissors") {
-    return "You lose";
+    return "You lose"; //scissors beats rock
   } else if (playerSelection === "rock" && computerSeleciton === "paper") {
-    return "You lose";
+    return "You lose"; // paper beats rock
   } else if (playerSelection === "paper" && computerSeleciton === "scissors") {
-    return "You lose";
+    return "You lose"; // scissors beats paper
   } else if (playerSelection === "rock" && computerSeleciton === "rock") {
-    return "It is a draw";
+    return "It is a draw"; // draw
   } else if (playerSelection === "paper" && computerSeleciton === "paper") {
-    return "It is a draw";
+    return "It is a draw"; // draw
   } else if (
     playerSelection === "scissors" &&
     computerSeleciton === "scissors"
   ) {
-    return "It is a draw";
+    return "It is a draw"; // draw
   } else {
-    return "Invalid player selection";
+    return "Invalid player selection"; // invalid selection
   }
 }
