eb2776e3d91bb597837771a268228ce994efb419
Majok-Bol <bolmajok001@gmail.com>
2023-11-23 17:58:45 +0300

    created a rockPaperScissors function with playerSelection and computerSelection parameters

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const myName="Abigael ";
-console.log(myName);
-const mySecondName="Lokook";
-console.log(mySecondName);
+function rockPaperScissors(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === "rock" && computerSeleciton === "scissors") {
+    return "You win";
+  } else if (playerSelection === "paper" && computerSeleciton === "rock") {
+    return "You win";
+  } else if (playerSelection === "scissors" && computerSeleciton === "paper") {
+    return "You win";
+  } else if (playerSelection === "rock" && computerSeleciton === "scissors") {
+    return "You lose";
+  } else if (playerSelection === "rock" && computerSeleciton === "paper") {
+    return "You lose";
+  } else if (playerSelection === "paper" && computerSeleciton === "scissors") {
+    return "You lose";
+  } else if (playerSelection === "rock" && computerSeleciton === "rock") {
+    return "It is a draw";
+  } else if (playerSelection === "paper" && computerSeleciton === "paper") {
+    return "It is a draw";
+  } else if (
+    playerSelection === "scissors" &&
+    computerSeleciton === "scissors"
+  ) {
+    return "It is a draw";
+  } else {
+    return "Invalid player selection";
+  }
+}
