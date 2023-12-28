a87bc478165d3ed42b6bdeb99f289111a9b7b7bf
Majok-Bol <bolmajok001@gmail.com>
2023-12-28 21:36:59 +0300

    added GUI to the game

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 function rockPaperScissors(playerSelection, computerSelection) {
-  playerSelection =
-    playerSelection.toLowerCase(); /* playerSelection is made case insensitive to enable the player input choice such as rock,Rock,ROCK,RocK etc */
-  if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win"; //rock beats scissors
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win"; // paper beats rock
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win"; // scissors beats paper
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You lose"; //scissors beats rock
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose"; // paper beats rock
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose"; // scissors beats paper
-  } else if (playerSelection === "rock" && computerSelection === "rock") {
-    return "It is a draw"; // draw
-  } else if (playerSelection === "paper" && computerSelection === "paper") {
-    return "It is a draw"; // draw
-  } else if (
-    playerSelection === "scissors" &&
-    computerSeleciton === "scissors"
+  // Make playerSelection case-insensitive
+  playerSelection = playerSelection.toLowerCase();
+
+  // Winning conditions
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return "It is a draw"; // draw
+    return "You win"; // Player wins
+  } else if (
+    // Losing conditions
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    return "You lose"; // Computer wins
+  } else if (playerSelection === computerSelection) {
+    return "It is a draw"; // Draw condition
   } else {
-    return "Invalid player selection"; // invalid selection
+    return "Invalid player selection"; // Invalid selection
   }
 }
+
 function getComputerChoice() {
   // Generate a random number between 0 and 2.
   const randomNum = Math.floor(Math.random() * 3);
@@ -34,32 +31,88 @@ function getComputerChoice() {
     return "rock";
   } else if (randomNum === 1) {
     return "paper";
-  }
-  if (randomNum === 2) {
+  } else if (randomNum === 2) {
     return "scissors";
   } else {
     return "Invalid selection";
     // something went wrong
   }
 }
-function game() {
-  const playerSelection = prompt("Enter your choice:Rock,Paper,Scissors");
-  // Get player's selection
+
+let round = 1;
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection) {
   const computerSelection = getComputerChoice();
-  // Get computer's selection
   const roundWinner = rockPaperScissors(playerSelection, computerSelection);
-  // Determine won the round
-  console.log(roundWinner);
-  // Display the results of the round
+
+  // Display results in the 'results' div
+  const resultsDiv = document.getElementById("results");
+
+  // Create a new paragraph element
+  const resultParagraph = document.createElement("p");
+
+  // Set the content of the paragraph based on the game result
+  resultParagraph.textContent = `Round ${round}: You chose ${playerSelection}, Computer chose ${computerSelection}. Result: ${roundWinner}`;
+
+  // Append the paragraph to the 'results' div
+  resultsDiv.appendChild(resultParagraph);
+
+  // Update the round counter
+  round++;
+  // Update scores based on the round result
   if (roundWinner === "You win") {
-    console.log("You won the round");
+    playerScore++;
   } else if (roundWinner === "You lose") {
-    console.log("You lost the round");
-  } else if (roundWinner === "It is a draw") {
-    console.log("The game is a draw");
-  } else {
-    console.log("Invalid move");
+    computerScore++;
+  }
+
+  // Check if all 6 rounds are completed
+  if (round > 5) {
+    // Display overall results
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.textContent = `Game over! Final scores:
+    - Player: ${playerScore}
+    - Computer: ${computerScore}
+  `;
+
+    // Determine and display the overall winner
+    const overallWinner =
+      playerScore > computerScore
+        ? "Player"
+        : computerScore > playerScore
+        ? "Computer"
+        : "It's a tie!";
+    resultsDiv.textContent += `\nOverall winner: ${overallWinner}`;
+
+    const restartButton = document.createElement("button");
+    restartButton.textContent = "Restart Game";
+    restartButton.onclick = restartGame;
+    resultsDiv.appendChild(restartButton);
   }
 }
-game();
-// Start the game
+// ... (existing code)
+
+function restartGame() {
+  // Reset the round counter
+  round = 1;
+
+  // Clear the contents of the 'results' div
+  const resultsDiv = document.getElementById("results");
+  while (resultsDiv.firstChild) {
+    resultsDiv.removeChild(resultsDiv.firstChild);
+  }
+
+  // Reset scores for a fresh game
+  playerScore = 0;
+  computerScore = 0;
+}
+
+// ... (existing code)
+
+if (round > 5) {
+  //
+
+  restartGame();
+}
